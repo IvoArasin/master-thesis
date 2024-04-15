88aaf95752303e486ca2689a72e3a022b5350039
IvoArasin <86802959+IvoArasin@users.noreply.github.com>
2024-04-15 15:10:55 +0200

    Update TVV.R

--- TVV.R
+++ TVV.R
@@ -1,10 +1,13 @@
 library(readxl)
+# import data
 data <- read_excel("/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/zero_rates08til23.xlsx")
+# create maturity vector
 m <- c(1/365, 7/365, 14/365, 1/12, 2/12, 3/12, 6/12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15)
 N <- 3
 scaler <- 1
 data <- as.matrix(subset(data, select=-c(1, 21, 22)))/scaler
 
+# Factor Loading matrix B
 factor_loadings <- function(l,m)
 {
   column1 <- rep.int(1,length(m))
@@ -15,9 +18,12 @@ factor_loadings <- function(l,m)
   B
 } 
 
+# Hyper parameters to create some statistics during optimization
 print_param_counter <<- 0
 total_iterations <<- 0
-independent_DNS_TVV <- function(para,Y,lik, h=0) {
+
+# DNS with time-varying common volatility component
+independent_DNS_TVV <- function(para,Y,lik=TRUE, h=0) {
   if(print_param_counter >=100){
     print(para)
     print_param_counter <<- 0
@@ -38,7 +44,9 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
   B	<- factor_loadings(l,m) 
   
   # Variance matrix of residuals
-  H <- diag(para[11:30])^2
+  start_ <- 11
+  end_ <- 11 + W - 1
+  H <- diag(para[start_:end_])^2
   
   # Vector autoregressive coeffient matrix: VAR(1)
   phi[1,1] <- para[2]
@@ -57,30 +65,35 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
   Q[3,3] <- para[10]
   Q[4,4] <- 0
   
+  # Take Square to ensure positivity
   Q <- Q %*% t(Q) 
   
-  v1   <- matrix(NA,T,W)			  
-  v2   <- matrix(NA,T,W) # Filtered errors: are defined as the difference between the observed yield curve and its filtered estimate from KF
-  
   # Gamma vector
-  Gamma <- abs(para[31:50])
+  start_ <- 11 + W
+  end_ <- 11 + W -1
+  Gamma <- abs(para[start_:end_])
   
   B <- cbind(B, Gamma)
   
   gamma0 <- 0.0001 #fixed for identifiability
-  gamma1 <- para[51]
-  gamma2 <- para[52]
+  end_ <- length(para)
   
+  gamma1 <- para[end_-1]
+  gamma2 <- para[end_]
+  
+  # Set up arrays to store recursive variables
   a.tt <- matrix(NA, T, N+1)
   a.t  <- matrix(NA, (T+1), N+1)
   P.tt <- array(NA, c(T, N+1, N+1))
   P.t  <- array(NA, c((T+1), N+1, N+1))
+  v1   <- matrix(NA,T,W)			  
+  v2   <- matrix(NA,T,W)
   
   # Start state vector and variance matrix
   a.t[1, 1:3] <- mu
   a.t[1, 4] <- 0
   
-  # Start variance matrix
+  # INitialize estimation error covariance matrix
   lyapunov<-function(N,phi,Q){
     matrix(solve(diag(N^2) - kronecker(phi,phi)) %*% matrix(Q,(N^2),1),N,N)
   }  
@@ -88,7 +101,7 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
   tryCatch(
     {
       cannot_be_done <- 0
-      P.t[1,1:3,1:3] <-lyapunov(N=N,phi=phi[1:3,1:3],Q=Q[1:3,1:3]) # Start variance matrix. Pt0
+      P.t[1,1:3,1:3] <-lyapunov(N=N,phi=phi[1:3,1:3],Q=Q[1:3,1:3])
     },
     warning = function(w){
       print(w)
@@ -122,11 +135,11 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
   h_t <- rep(T, 0)
   
   # Initial log-likelihood	
-  logLik <- 1
+  logLik <- 0
   
   for (t in 1:T) 
   {
-    # Restrictions
+    # Restrictions on Gamma
     if(gamma0 <= 0 || gamma1<=0 || gamma2<=0 || gamma1+gamma2>=1){
       print("Gamma error")
       logLik<- -1000000000000000;
@@ -138,6 +151,7 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
     F <- B %*% P.t[t, ,] %*% t(B) + H # prediction error variance matrix
     
     detF <- det(F)
+    # Ensure invertibility of F
     if(is.na(detF) || is.nan(detF) || is.infinite(detF) || abs(detF)<1e-190 || detF<0){
       print("detF cannot be determined")
       logLik<- -1000000000000000
@@ -214,8 +228,11 @@ independent_DNS_TVV <- function(para,Y,lik, h=0) {
   }
 }
 
+##################################################################
+##################################################################
+##################################################################
 
-para_init <- c( # derived from two-step method
+para_init_TVV <- c( # derived from two-step method
   0.36,
   0.98, 0.97, 0.93,
   3.2, -1.6, -2.4,
@@ -229,27 +246,45 @@ para_init <- c( # derived from two-step method
   
   0.4, 0.4)
 
-automated_rollingWindow(para_init=para_init, model=independent_DNS_TVV, start_=25, end_=c(106, 111, 116, 121, 126, 131), maxiter=15)
+# Automatically optimize model in recursive fashion (Iterative Nelder-Mead)
+myDirectory = "/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/optimized_files/RollingWindowForcasts"
+automated_rollingWindow(para_init=para_init_TVV, model=independent_DNS_TVV, maxiter=15, maxFuncEvals=2000, data=data, directoryPath=myDirectory, method_="BFGS")
 
-automated_rollingWindow <- function(para_init, model, start_, end_, maxiter=5){
+para_optim <- as.matrix(read.csv("/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/optimized_files/RollingWindowForcastsTVV_1_final.csv"))
+params_ <- para_optim[1,5:56]
+TVV_model_object <- independent_DNS_TVV(para=params_, Y=data, lik=FALSE, h=12)
+
+# Plot common volatility variance h_t
+plot(TVV_model_object$h_t[10:191], type="l", main=expression(paste("Common Volatility ", h[t])))
+
+# plot Gamma weights
+plot(m, params_[31:50],
+     type="b",
+     main=expression(paste("6 Common Volatility Weights ", Gamma[epsilon])))
+
+# This function can be used to recursively optimize expanding windows
+automated_rollingWindow <- function(para_init, model, start_=1, end_=0, data=data, maxiter=10, maxFuncEvals=100000, directoryPath="", method_="Nelder-Mead"){
   starting_values <- para_init
   start <- start_
+  if(end_==0){
+    end_ <- nrow(data)
+  }
   for(i in 1:length(end_)){
     end <- end_[i]
     window_data <- data[start:end,]
-    starting_values <- automatic_optimization(starting_values, model, window_data, maxiter=maxiter, window_nr=i)
+    starting_values <- automatic_optimization(starting_values, model, window_data, maxiter=maxiter, window_nr=i, directoryPath=directoryPath, maxFuncEvals=maxFuncEvals, method_=method_)
     preds <- model(para=starting_values, Y=window_data, lik=FALSE,h=12)
     preds <- preds$y_t_h
     preds_h12 <- preds[12,]
     preds_h6 <- preds[6,]
     preds_h1 <- preds[1,]
     predictions <- rbind(preds_h12, preds_h6, preds_h1)
-    myDirectory = "/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/optimized_files/CalmTimeRollingWindow/"
+    myDirectory = directoryPath
     write.csv(predictions, paste0(myDirectory,"predictions_window_", i, "TVV.csv"), row.names=TRUE)
   }
 }
 
-automatic_optimization <- function(para_init, model, input_data, maxiter=5, window_nr=0){
+automatic_optimization <- function(para_init, model, input_data, maxiter=10, window_nr=0, directoryPath="", maxFuncEvals=maxFuncEvals, method_="Nelder-Mead"){
   maxiter <- maxiter
   parameter_length <- length(para_init)
   model_parameters <- matrix(NA, maxiter, parameter_length)
@@ -261,85 +296,51 @@ automatic_optimization <- function(para_init, model, input_data, maxiter=5, wind
   prevLogLike <- 0
   logLike <- 2
   i<-0
+  if(method_=="BFGS"){
+    method_ <- "BFGS"
+    maxiter <- 1
+  }
   while(abs(prevLogLike-logLike)>0.1 & i<maxiter){
     i<-i+1
     
     # optimize model parameters
-    optim_values<-optim(parameters,model,control = list(trace=1, maxit=100000), Y=input_data, lik=TRUE, h=0)
-    print("HERE")
-    print(nrow(input_data))
+    optim_values<-optim(parameters,model,control = list(trace=1, maxit=maxFuncEvals), Y=input_data, method=method_)
+    
     #convergence
     optim_statistics[i,1] <- optim_values$convergence
     
     #iterations
     optim_statistics[i,2] <- total_iterations
     
-    # NegLogLike
-    prevLogLike <- logLike
-    logLike <- model(para=parameters, Y=input_data, lik=TRUE, h=0)
-    optim_statistics[i,3] <- logLike
-    
     # model parameters
     model_parameters[i, ] <- optim_values$par
     parameters <- optim_values$par
     
+    # NegLogLike
+    prevLogLike <- logLike
+    logLike <- model(para=parameters, Y=input_data, lik=TRUE)
+    optim_statistics[i,3] <- logLike
+    
     # filtered RMSE
-    model_output <- model(para=parameters, Y=input_data, lik=FALSE, h=0)
+    model_output <- model(para=parameters, Y=input_data, lik=FALSE)
     
     filtered_error_RMSE[i, ] <- sqrt(colMeans((model_output$v2)^2))
     
     total_iterations <<- 0
     
-    myDirectory = "/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/optimized_files/CalmTimeRollingWindow/"
-    fileName <- paste0(myDirectory,"TVV_calmTime_window_intermediate.csv")
+    myDirectory = directoryPath
+    fileName <- paste0(myDirectory,"TVV_intermediate.csv")
     intermediate_result <- list("optim_statistics"=optim_statistics, "model_parameters"=model_parameters, "filtered_error_RMSE"=filtered_error_RMSE)
     write.csv(intermediate_result, file=fileName, row.names = TRUE)
   }
-  fileName_finalOutput <- paste0(myDirectory,"TVV_calmTime_window_", window_nr, "_final.csv")
-  #result <- list("model_parameters"=model_parameters)
+  fileName_finalOutput <- paste0(myDirectory,"TVV_", window_nr, "_final.csv")
   result <- list("optim_statistics"=optim_statistics, "model_parameters"=model_parameters, "filtered_error_RMSE"=filtered_error_RMSE)
   write.csv(result, fileName_finalOutput, row.names=TRUE)
   return(optim_values$par)
 }
-
-parameters <- read.csv2("/Users/ivoarasin/Desktop/Master/Semester Four/thesis/master_thesis_code_R/Finished DNS model files/optimized_files/RollingWindowForcasts/full_sample_parameters/TVV_fullSample.csv")
-para_fullSamle_optim <- as.numeric(parameters[8,5:56])
-
 # Convert Log Likelihood of MLE estimates between models with differently scaled data
 para_scaler <- function(parameters){
   parameters_Scaled <- parameters
   parameters_Scaled[5:50] <- parameters_Scaled[5:50]/100
   parameters_Scaled
 }
-
-# apply model
-time_ <- 131
-k_obj_TVV <- independent_DNS_TVV(DNS_TVV_window_3_bfgs_normal$par, Y=train_data[25:time_,], lik=FALSE, h=12)
-k_obj_TVV$negLogLik
-plot(k_obj_TVV$h_t, type="l", main=expression(paste("Common Volatility ", h[t])))
-
-plot(m, data[(time_+12),], type="l")
-lines(m, data[(time_),], type="l", col="red", lty=2)
-lines(m,k_obj_TVV$y_t_h[12,], type="l", lty=2, col="blue")
-
-# Observe Predictive Tendency for 12mo ahead forecast
-for(i in 1:12){
-  if(i==1){plot(m, k_obj_TVV$y_t_h[i,], type="l",lwd=2)
-    lines(m, data[(time_),], type="l", col="red", lty=2)
-    lines(m, data[(time_+12),], type="l", col="blue", lty=2)
-    }
-  else{
-    lines(m, k_obj_TVV$y_t_h[i,], type="l")
-  }
-}
-
-# Reconstruct Yield Curve with Model
-time_ <- 75
-recon <- factor_loadings(para_init[1], m)%*%k_obj_TVV$a.tt[time_,1:3]+para_init[31:50]*k_obj_TVV$a.tt[time_,4]
-plot(m, data[(time_),], type="l")
-lines(m, recon, type="l", lty=2, col="blue")
-
-# plot Gamma weights
-plot(c(1/365, 7/365, 14/365, 1/12, 2/12, 3/12, 6/12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15), abs(DNS_TVV_window_1_bfgs_normal$par[31:50]), type="b", main=expression(paste("6 Common Volatility Weights ", Gamma[epsilon])))
-# plot volatility variance
-plot(k_obj_TVV$h_t, type="l", main=expression(paste("Common Volatility ", h[t])))
